b9641ad161643865735aa71026319964686f6412
admin <hui.huang@56qq.com>
2018-09-18 10:26:58 +0800

    feat:访问数据库配置

--- config/config.default.js
+++ config/config.default.js
@@ -1,10 +1,32 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  // const config = exports = {};
+  const config = {
+      mysql: {
+        // 单数据库信息配置
+        client: {
+          // host
+          host: 'localhost',
+          // 端口号
+          port: '3306',
+          // 用户名
+          user: 'root',
+          // 密码
+          // password: 'huanghui0330',
+          password: 'huanghui0330',
+          // 数据库名
+          database: 'ant_admin',
+        },
+        // 是否加载到 app 上，默认开启
+        app: true,
+        // 是否加载到 agent 上，默认关闭
+        agent: false,
+      }
+    };
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1537175438338_1126';
+  config.keys = appInfo.name + '_1537199351611_7126';
 
   // add your config here
   config.middleware = [];
--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,10 @@
 
 // had enabled by egg
 // exports.static = true;
+
+
+// config/plugin.js
+exports.mysql = {
+    enable: true,
+    package: 'egg-mysql',
+};
